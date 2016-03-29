e0599b835fd599d09e4f184257c216350f1aeb08
Gaurav Mahapatra <gm-20.github.io>
2016-03-29 21:55:05 +0530

    First commit

---
+++ server.js
@@ -0,0 +1,51 @@
+
+var mongo = require('mongodb').MongoClient,
+   client = require('socket.io').listen(8080).sockets;
+
+
+
+   mongo.connect('mongodb://127.0.0.1/chat',function(err,db){
+        if(err) throw err;
+    
+    client.on('connection', function(socket){
+
+    	var col=db.collection('messages'),
+            sendStatus = function (s){
+                socket.emit('status',s);
+
+            };
+
+
+            col.find().limit(50).sort({_id:1}).toArray(function  (err,res) {
+                if(err) throw err;
+                socket.emit('output',res);
+            });
+
+    	socket.on('input',function(data){
+           var name = data.name;
+            var message = data.message;
+            whitespacePattern = /^\s*$/;
+
+            if(whitespacePattern.test(name) || whitespacePattern.test(message)){
+            	sendStatus('Name and message required');
+
+            }
+            else{
+            	col.insert({name: name,message: message},function(){
+                
+                client.emit('output',[data]);
+
+            	sendStatus({
+                    message: "Message Sent",
+                    clear : true
+                });
+            });
+
+            }
+
+            
+    	});
+    });
+
+   });
+ 
